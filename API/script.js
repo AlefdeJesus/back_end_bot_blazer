dba4679613d476242bb0009d574272c3f56e89bb
AlefdeJesus <alefjesus1996st.s@gmail.com>
2023-10-17 10:30:57 -0300

    TEST1

--- API/script.js
+++ API/script.js
@@ -5,12 +5,12 @@ const sequelize = require('sequelize');
 const {Sinais} = require('./models');
 
 const arrayResultado = [];
-const browser = await puppeteer.launch({
-  executablePath: '/usr/bin/google-chrome',
-  args: ['--no-sandbox', '--disable-setuid-sandbox'],
-  headless: true,
-});
-
+async function realizarScraping() {
+  const browser = await puppeteer.launch({
+    executablePath: '/usr/bin/google-chrome', // Caminho para o Chrome no ambiente Vercel
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    headless: true,
+  });
   const page = await browser.newPage();
 
   await page.goto('https://blaze-4.com/pt/games/double', { timeout: 60000 });
