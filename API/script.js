b4e1fdbb4d3f628a7210e27b259ebdad23829394
AlefdeJesus <alefjesus1996st.s@gmail.com>
2023-10-17 10:19:47 -0300

    VERCEL

--- API/script.js
+++ API/script.js
@@ -5,8 +5,12 @@ const sequelize = require('sequelize');
 const {Sinais} = require('./models');
 
 const arrayResultado = [];
-async function realizarScraping(cor, numero) {
-  const browser = await puppeteer.launch();
+async function realizarScraping() {
+  const browser = await puppeteer.launch({
+    executablePath: '/vercel/path/to/chrome',
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    headless: true,
+  });
   const page = await browser.newPage();
 
   await page.goto('https://blaze-4.com/pt/games/double', { timeout: 60000 });
