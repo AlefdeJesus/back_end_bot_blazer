a136267b800d4831666e9fa2f69e5dffb4940df4
AlefdeJesus <alefjesus1996st.s@gmail.com>
2023-10-17 09:59:16 -0300

    subir para a vercel

--- API/config/config.js
+++ API/config/config.js
@@ -1,29 +1,58 @@
 const postgres = require('postgres');
+const pg = require('pg');
+require('dotenv').config();
 
-const config = {
-  development: {
-    username: "admin",
-    password: "admin",
-    database: "meubanco",
-    host: "localhost",
-    dialect: 'postgres',
-    port: 5432,
-   /* dialectOptions: { username: "admin",
-      password: "admin",
-      database: "meubanco",
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
+  var username = process.env.USE;
+  var password = process.env.PASSWORD;
+  var database = process.env.DATABASE;
+  var host = process.env.HOST;
+  var dialect = process.env.DIALECT;
+
+  const config = {
+    development: {
+      username: username,
+      password: password,
+      database: database,
+      host: host,
+      dialect: 'postgres',
+      port: 5432,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      },
+      sslmode: "require"
     },
-    sslmode: "require"*/
-
-  },
-  test: {
-    
-  },
-  production: {
-    
-  }
-};
-module.exports = config;
+    test: {
+      username: username,
+      password: password,
+      database: database,
+      host: host,
+      dialect: 'postgres',
+      port: 5432,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      },
+      sslmode: "require"
+    },
+    production: {
+      username: username,
+      password: password,
+      database: database,
+      host: host,
+      dialect: 'postgres',
+      port: 5432,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      },
+      sslmode: "require"
+    }
+  };
+  module.exports = config;
